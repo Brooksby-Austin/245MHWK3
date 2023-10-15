dcc317859a8ad396afd176d3501b030cf64f7657
Brooksby-Austin <brooksby.austin@gmail.com>
2023-10-15 11:51:57 -0700

    2a done

---
+++ assignment_3.R
@@ -0,0 +1,24 @@
+rm(list=ls()) # clear the environment
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+#-------Import necessary packages here-------------------#
+# Note: Using other packages than the ones listed below can break the autograder
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+library(janitor)
+#------ Uploading PERMID --------------------------------#
+PERMID <- "7216567" #Type your PERMID within the quotation marks
+PERMID <- as.numeric(gsub("\\D", "", PERMID)) #Don't touch
+set.seed(PERMID) #Don't touch
+#------ Answer ------------------------------------------#
+
+#1
+uof <- read.csv("uof_louisville.csv")
+
+#2
+#a
+uof <- uof |> mutate(hour = hour(hms(time_of_occurrence)))
+frequent_hour <- uof |> count(hour) |> arrange(desc(hour)) |> slice(1)
+frequent_hour <- frequent_hour$hour
+
+#b
